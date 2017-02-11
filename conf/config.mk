00532140f350e359a4f81b3d5968ea5827c61e1a
Elvis Wang <mail@wbprime.me>
2017-02-11 22:57:34 +0800

    Update makefile rules

--- conf/config.mk
+++ conf/config.mk
@@ -24,10 +24,10 @@ CLEAN_SUBDIRS = $(addprefix clean_,$(SUBDIRS))
 .PHONY: clean_subdirs $(CLEAN_SUBDIRS)
 clean_subdirs: $(CLEAN_SUBDIRS)
 $(CLEAN_SUBDIRS):
-	$(MAKE) -C $@ clean
+	$(MAKE) -C $(subst clean_,,$@) clean
 
 INSTALL_SUBDIRS = $(addprefix install_,$(SUBDIRS))
 .PHONY: install_subdirs $(INSTALL_SUBDIRS)
 install_subdirs: $(INSTALL_SUBDIRS)
 $(INSTALL_SUBDIRS):
-	$(MAKE) -C $@ install
+	$(MAKE) -C $(subst install_,,$@) install
